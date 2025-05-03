64583338c061422a855fce45bd08b4ba9dc21713
Roi-flores13 <roi.flores@iteso.mx>
2025-05-02 20:05:40 -0600

    Documentación del código

--- project_pipeline/api_call.py
+++ project_pipeline/api_call.py
@@ -10,6 +10,11 @@ headers = { 'X-Auth-Token':  os.environ.get("API_TOKEN")}
 
 response = requests.get(uri, headers=headers)
 
+"""
+Falta el análisis de cómo funcionan los archivos de la API.
+(¿qué se extrae?, ¿dónde consigo los ids?, ¿qué información es util?, etc)
+"""
+
 # with open("../data/raw/matches_data_raw.json", "w") as f:
 #     for match in response.json()["matches"]:
 #         json.dump(match, f, indent=4)
--- project_pipeline/snowflake_job.py
+++ project_pipeline/snowflake_job.py
@@ -7,12 +7,15 @@ load_dotenv()
 
 class snowflake_connector:
     def __init__(self) -> None:
+        
+        # Extracción de las credenciales desde el .env
         PASSWORD = os.getenv("SNOWFLAKE_PASSWORD")
         USER = os.getenv("SNOWFLAKE_USER")
         ACCOUNT = os.getenv("SNOWFLAKE_ACCOUNT")
         SCHEMA = os.getenv("SCHEMA")
         DATABASE = os.getenv("DATABASE")
         
+        # Conexión a Snowflake mediante las credenciales
         self.conn = snowflake.connector.connect(
         user=USER,
         password=PASSWORD,
@@ -23,19 +26,22 @@ class snowflake_connector:
         
         print("conexión establecida con Snowflake")
         
-    def upload_to_snowflake_datalake(self, local_file_path, stage_name='@DATALAKE_FUTBOL', connection='my_connection'):
-        file_path = Path(local_file_path).resolve()
-        file_path = file_path.as_posix()
+    def upload_to_snowflake_datalake(self, local_file_path:str, stage_name='@DATALAKE_FUTBOL', connection='my_connection') -> None:
+        
+        file_path = Path(local_file_path).resolve() # Extrae el path del archivo a subir
+        file_path = file_path.as_posix() # Cambia las diagonales para que no haya errores
 
+        # Armar el comando que se escribiría en la terminal para hacer el PUT en Snowflake 
         command = [
             'snowsql',
             '-c', connection,
             '-q', f"PUT file://{file_path} {stage_name} AUTO_COMPRESS=FALSE"
         ]
 
+        # Se intenta subir el archivo al DataLake de SnowFlake
         try:
             print(f"Ejecutando: {' '.join(command)}")
-            result = subprocess.run(command, capture_output=True, text=True, check=True)
+            result = subprocess.run(command, capture_output=True, text=True, check=True) # Se corre el comando como si fuera en la terminal
             print("Subido correctamente:")
             print(result.stdout)
             
