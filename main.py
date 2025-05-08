2f4a1bd21e310e81979d9a4d1fff32de90838aa3
Roi-flores13 <roi.flores@iteso.mx>
2025-05-08 17:40:56 -0600

    Se añadieron la funciones al main

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
-from project_pipeline.api_call import extract_api_data, api_data_eraser
+from project_pipeline.api_call import api_manager
 from project_pipeline.snowflake_job import snowflake_connector
 
 
 if __name__ == "__main__":
-    print("Iniciando extracción")
-    extract_api_data()
-    print("Iniciando conexión")
-    sc = snowflake_connector()
-    print("Subiendo datos")
-    sc.upload_to_snowflake_datalake("data/temp_raw")
-    # sc.erase_everything_from_datalake()
+    AM = api_manager()
+    SC = snowflake_connector()
+
+    # AM.extract_api_data()
+    # SC.upload_to_snowflake_datalake()
+    
+    SC.erase_everything_from_datalake()
+    
+    # SC.extraer_tabla_desde_json_stage()
+    # api_manager.api_data_eraser("data/processed")
