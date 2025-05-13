ba035d7d4640221bf68d300c31fbc02f6ca2c648
Roi-flores13 <roi.flores@iteso.mx>
2025-05-12 19:00:53 -0600

    Pipeline completo y corriendo

--- main.py
+++ main.py
@@ -6,10 +6,11 @@ if __name__ == "__main__":
     AM = api_manager()
     SC = snowflake_connector()
 
-    # AM.extract_api_data()
-    # SC.upload_to_snowflake_datalake()
+    AM.extract_api_data()
+    SC.upload_to_snowflake_datalake()  
+    SC.extraer_tabla_desde_json_stage()
+    SC.create_tables()
+    SC.upload_data_to_table()
     
-    SC.erase_everything_from_datalake()
-    
-    # SC.extraer_tabla_desde_json_stage()
-    # api_manager.api_data_eraser("data/processed")
+    # SC.erase_everything_from_datalake()
+    # api_manager.api_data_eraser()
